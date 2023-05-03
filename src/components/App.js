a1aa27d166e08887a99bb4090735027ec6e30862
Fredrick <mwangikingz94@gmail.com>
2023-05-03 20:25:53 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, {useState}  from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+const [isDark, SetToDark] = useState(false)
+
+function changeColor(){
+  SetToDark(!isDark)
+}
+
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
@@ -12,7 +18,7 @@ function App() {
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={changeColor}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,22 @@
-import React from "react";
+
+ 
+
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [category, setCategory] = useState("All");
+
+  function selectedCategory(event) {
+    setCategory(event.target.value);
+  }
+
+  const filteredItems = category === "All" ? items : items.filter(item => item.category === category);
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" value={category} onChange={selectedCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +24,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
